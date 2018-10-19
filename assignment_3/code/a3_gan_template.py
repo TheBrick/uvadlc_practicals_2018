74e115d515654653eb76866bf310943eaea10303
Unknown <stickfiguredaan@gmail.com>
2018-10-19 08:45:34 +0200

    Make and run all the things

--- assignment_3/code/a3_gan_template.py
+++ assignment_3/code/a3_gan_template.py
@@ -6,6 +6,7 @@ import torch.nn as nn
 import torchvision.transforms as transforms
 from torchvision.utils import save_image
 from torchvision import datasets
+import matplotlib.pyplot as plt
 
 
 class Generator(nn.Module):
@@ -27,10 +28,26 @@ class Generator(nn.Module):
         #   LeakyReLU(0.2)
         #   Linear 1024 -> 768
         #   Output non-linearity
+        self.generator = nn.Sequential(
+            nn.Linear(args.latent_dim, 128),
+            nn.LeakyReLU(0.2),
+            nn.Linear(128, 256),
+            nn.BatchNorm1d(256),
+            nn.LeakyReLU(0.2),
+            nn.Linear(256, 512),
+            nn.BatchNorm1d(512),
+            nn.LeakyReLU(0.2),
+            nn.Linear(512, 1024),
+            nn.BatchNorm1d(1024),
+            nn.LeakyReLU(0.2),
+            nn.Linear(1024, 784),
+            nn.Tanh()
+        )
 
     def forward(self, z):
         # Generate images from z
-        pass
+        img = self.generator(z)
+        return img
 
 
 class Discriminator(nn.Module):
@@ -45,24 +62,60 @@ class Discriminator(nn.Module):
         #   LeakyReLU(0.2)
         #   Linear 256 -> 1
         #   Output non-linearity
+        self.discriminator = nn.Sequential(
+            nn.Linear(784, 512),
+            nn.LeakyReLU(0.2),
+            nn.Linear(512, 256),
+            nn.LeakyReLU(0.2),
+            nn.Linear(256, 1),
+            nn.Sigmoid()
+        )
 
     def forward(self, img):
         # return discriminator score for img
-        pass
+        probability_of_real_img = self.discriminator(img)
+        return probability_of_real_img
 
 
-def train(dataloader, discriminator, generator, optimizer_G, optimizer_D):
+def save_GAN_plot(G_curve, D_curve, filename):
+    plt.figure(figsize=(12, 6))
+    line1 = plt.plot(G_curve, label='G loss')
+    line2 = plt.plot(D_curve, label='D loss')
+    plt.setp(line1, linewidth=0.25)
+    plt.setp(line2, linewidth=0.25)
+    plt.legend()
+    plt.xlabel('iterations')
+    plt.ylabel('loss')
+    plt.tight_layout()
+    plt.savefig(filename)
+
+def train(dataloader, D, G, optimizer_G, optimizer_D):
+    D_curve, G_curve = [], []
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
     for epoch in range(args.n_epochs):
         for i, (imgs, _) in enumerate(dataloader):
-
-            imgs.cuda()
+            # Prepare real images
+            X = imgs.view(-1, 784).to(device)
 
             # Train Generator
             # ---------------
+            Z = torch.randn(X.shape[0], args.latent_dim, device=device)
+            loss_G = -torch.log(D(G(Z))).sum()
+            optimizer_G.zero_grad()
+            loss_G.backward()
+            optimizer_G.step()
 
             # Train Discriminator
             # -------------------
+            Z = torch.randn(X.shape[0], args.latent_dim, device=device)  # resample Z
+            loss_D = -(torch.log(D(X)) + torch.log(1 - D(G(Z)))).sum()
             optimizer_D.zero_grad()
+            loss_D.backward()
+            optimizer_D.step()
+
+            # Save them for later
+            G_curve.append(loss_G)
+            D_curve.append(loss_D)
 
             # Save Images
             # -----------
@@ -71,10 +124,12 @@ def train(dataloader, discriminator, generator, optimizer_G, optimizer_D):
                 # You can use the function save_image(Tensor (shape Bx1x28x28),
                 # filename, number of rows, normalize) to save the generated
                 # images, e.g.:
-                # save_image(gen_imgs[:25],
-                #            'images/{}.png'.format(batches_done),
-                #            nrow=5, normalize=True)
-                pass
+                save_image(G(Z[:25]).view(-1, 1, 28, 28),
+                           'images/{}.png'.format(batches_done),
+                           nrow=5, normalize=True)
+                print("Epoch:{} Iteration:{} loss_D:{:.2f} loss_G:{:.2f}".format(epoch, i, loss_D, loss_G))
+
+    save_GAN_plot(G_curve, D_curve, 'GAN.pdf')
 
 
 def main():
@@ -90,9 +145,11 @@ def main():
                                                 (0.5, 0.5, 0.5))])),
         batch_size=args.batch_size, shuffle=True)
 
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
     # Initialize models and optimizers
-    generator = Generator()
-    discriminator = Discriminator()
+    generator = Generator().to(device)
+    discriminator = Discriminator().to(device)
     optimizer_G = torch.optim.Adam(generator.parameters(), lr=args.lr)
     optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=args.lr)
 
@@ -101,7 +158,7 @@ def main():
 
     # You can save your generator here to re-use it to generate images for your
     # report, e.g.:
-    # torch.save(generator.state_dict(), "mnist_generator.pt")
+    torch.save(generator.state_dict(), "mnist_generator_epoch={}.pt".format(args.n_epochs))
 
 
 if __name__ == "__main__":
--- assignment_3/code/a3_vae_template.py
+++ assignment_3/code/a3_vae_template.py
@@ -4,6 +4,7 @@ import torch
 import torch.nn as nn
 import matplotlib.pyplot as plt
 from torchvision.utils import make_grid
+import math
 
 from datasets.bmnist import bmnist
 
@@ -12,6 +13,16 @@ class Encoder(nn.Module):
 
     def __init__(self, hidden_dim=500, z_dim=20):
         super().__init__()
+        self.encoder_mean = nn.Sequential(
+            nn.Linear(784, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, z_dim)
+        )
+        self.encoder_std = nn.Sequential(
+            nn.Linear(784, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, z_dim)
+        )
 
     def forward(self, input):
         """
@@ -20,9 +31,8 @@ class Encoder(nn.Module):
         Returns mean and std with shape [batch_size, z_dim]. Make sure
         that any constraints are enforced.
         """
-        mean, std = None, None
-        raise NotImplementedError()
-
+        mean = self.encoder_mean(input)
+        std = self.encoder_std(input)
         return mean, std
 
 
@@ -30,6 +40,12 @@ class Decoder(nn.Module):
 
     def __init__(self, hidden_dim=500, z_dim=20):
         super().__init__()
+        self.decoder = nn.Sequential(
+            nn.Linear(z_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, 784),
+            nn.Sigmoid()
+        )
 
     def forward(self, input):
         """
@@ -37,28 +53,59 @@ class Decoder(nn.Module):
 
         Returns mean with shape [batch_size, 784].
         """
-        mean = None
-        raise NotImplementedError()
+        mean = self.decoder(input)
 
         return mean
 
 
 class VAE(nn.Module):
 
-    def __init__(self, hidden_dim=500, z_dim=20):
+    def __init__(self, hidden_dim=500, z_dim=20, device='cpu'):
         super().__init__()
 
         self.z_dim = z_dim
-        self.encoder = Encoder(hidden_dim, z_dim)
-        self.decoder = Decoder(hidden_dim, z_dim)
+        self.encoder = Encoder(hidden_dim, z_dim).to(device)
+        self.decoder = Decoder(hidden_dim, z_dim).to(device)
+        self.device = device
+
+    def log_bernoulli_loss (self, x, r):
+        e = 1e-8  # nonzerofier
+        losses = - torch.sum(x * torch.log(r+e) + (1 - x) * torch.log(1 - r+e), dim=1)
+        return losses
+
+    def KL_loss (self, mu, sigma):
+        # sum over components and data points at the same time (summing loss of each data point)
+        wip = 1 + torch.log(sigma.pow(2)) - mu.pow(2) - sigma.pow(2)
+        divergence = torch.sum(wip, dim=1) / 2
+        return -divergence
 
     def forward(self, input):
         """
         Given input, perform an encoding and decoding step and return the
         negative average elbo for the given batch.
         """
-        average_negative_elbo = None
-        raise NotImplementedError()
+        # Bark bark
+        input = input.view(-1, 784).to(self.device)
+
+        # Encode input onto Z
+        mean, std = self.encoder(input)
+
+        # Sample Z with reparametrization trick
+        epsilons = torch.randn((1, self.z_dim), device=self.device)
+        z = mean + std * epsilons
+
+        # Reconstruct inputs with decoder
+        reconstructions = self.decoder(z)
+
+        # Calculate the reconstruction loss
+        reconstruction_losses = self.log_bernoulli_loss(input, reconstructions)
+
+        # Calculate the regularization loss
+        regularization_losses = self.KL_loss(mean, std)
+
+        # Calculate ELBO
+        average_negative_elbo = torch.mean(reconstruction_losses + regularization_losses, dim=0)
+
         return average_negative_elbo
 
     def sample(self, n_samples):
@@ -67,10 +114,21 @@ class VAE(nn.Module):
         (from bernoulli) and the means for these bernoullis (as these are
         used to plot the data manifold).
         """
-        sampled_ims, im_means = None, None
-        raise NotImplementedError()
+        # Sample Z without transforming prior
+        z = torch.randn((n_samples, self.z_dim), device=self.device)
 
-        return sampled_ims, im_means
+        # Construct samples with decoder
+        sampled_ims = self.decoder(z)
+
+        # Calculate means
+        im_means = sampled_ims.mean(dim=0)
+
+        # Convert to binary to get actual samples, but let's not do it here to
+        # get more insight into the decoder from the images
+        # sampled_ims = (samples > 0.5) * 255
+
+
+        return sampled_ims.view(n_samples, 1, 28, 28), im_means.view(1, 1, 28, 28)
 
 
 def epoch_iter(model, data, optimizer):
@@ -80,10 +138,18 @@ def epoch_iter(model, data, optimizer):
 
     Returns the average elbo for the complete epoch.
     """
-    average_epoch_elbo = None
-    raise NotImplementedError()
+    total_elbo = 0
 
-    return average_epoch_elbo
+    for i, batch in enumerate(data):
+        avg_elbo = model(batch)
+        if model.training:
+            model.zero_grad()
+            avg_elbo.backward()
+            optimizer.step()
+        total_elbo += avg_elbo.item()
+
+    avg_epoch_elbo = total_elbo / len(data)
+    return avg_epoch_elbo
 
 
 def run_epoch(model, data, optimizer):
@@ -111,11 +177,37 @@ def save_elbo_plot(train_curve, val_curve, filename):
     plt.tight_layout()
     plt.savefig(filename)
 
+def sample_model (model, epoch, train_elbo, val_elbo):
+    with torch.no_grad():
+        row_count = 5
+        samples, im_means = model.sample(row_count * row_count)
+        samples = make_grid(samples, nrow=row_count)
+        path = "samples/z={}_epoch={}_trainingloss={:.3f}_validationloss={:.2f}.png".format(
+            ARGS.zdim,
+            epoch,
+            train_elbo,
+            val_elbo
+        )
+        plt.imsave(path, samples.cpu().numpy().transpose(1, 2, 0))
+
+def plot_manifold (model):
+    with torch.no_grad():
+        row_count = 20
+        grid = torch.linspace(0, 1, row_count)
+        samples = [torch.erfinv(2 * torch.tensor([x, y]) - 1) * math.sqrt(2) for x in grid for y in grid]
+        samples = torch.stack(samples).to(model.device)
+        manifold = model.decoder(samples).view(-1, 1, 28, 28)
+        img = make_grid(manifold, nrow=row_count)
+        plt.imsave("manifold.png", img.cpu().numpy().transpose(1, 2, 0))
 
 def main():
     data = bmnist()[:2]  # ignore test split
-    model = VAE(z_dim=ARGS.zdim)
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model = VAE(z_dim=ARGS.zdim, device=device)
     optimizer = torch.optim.Adam(model.parameters())
+
+    # sample before training
+    sample_model(model, 0, 9999, 9999)
 
     train_curve, val_curve = [], []
     for epoch in range(ARGS.epochs):
@@ -129,12 +221,15 @@ def main():
         #  Add functionality to plot samples from model during training.
         #  You can use the make_grid functioanlity that is already imported.
         # --------------------------------------------------------------------
+        sample_model(model, epoch, train_elbo, val_elbo)
 
     # --------------------------------------------------------------------
     #  Add functionality to plot plot the learned data manifold after
     #  if required (i.e., if zdim == 2). You can use the make_grid
     #  functionality that is already imported.
     # --------------------------------------------------------------------
+    if ARGS.zdim == 2:
+        plot_manifold(model)
 
     save_elbo_plot(train_curve, val_curve, 'elbo.pdf')
 
